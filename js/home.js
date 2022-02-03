ed3a50bdcbb03045284e8f0dbbc777b79c3a5a05
Chirag Singh <chirag.s@360logica.com>
2022-02-03 15:07:37 +0530

    Change the index.html text

---
+++ js/home.js
@@ -0,0 +1,11 @@
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("card");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 1000); 
+  }
